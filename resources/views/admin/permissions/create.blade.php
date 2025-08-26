4c6a7d93c8c060227372c478b965cd156173ce6d
Angga Pratama <angga.pratama579@gmail.com>
2025-08-26 04:34:45 +0000

    🎨 style(permission): update fresh UI pada bagian permission

--- resources/views/admin/permissions/create.blade.php
+++ resources/views/admin/permissions/create.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="card">
+<div class="card form-card">
     <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.permission.title_singular') }}
+        <h4 class="mb-0">{{ trans('global.create') }} {{ trans('cruds.permission.title_singular') }}</h4>
     </div>
 
     <div class="card-body">
         <form method="POST" action="{{ route("admin.permissions.store") }}" enctype="multipart/form-data">
             @csrf
-            <div class="form-group">
-                <label class="required" for="title">{{ trans('cruds.permission.fields.title') }}</label>
+            <div class="form-group mb-3">
+                <label class="form-label required" for="title">{{ trans('cruds.permission.fields.title') }}</label>
                 <input class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" type="text" name="title" id="title" value="{{ old('title', '') }}" required>
                 @if($errors->has('title'))
                     <div class="invalid-feedback">
@@ -19,15 +19,15 @@
                 @endif
                 <span class="help-block">{{ trans('cruds.permission.fields.title_helper') }}</span>
             </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
+            
+            <div class="card-footer text-end">
+                <a href="{{ route('admin.permissions.index') }}" class="btn btn-secondary me-2">Batal</a>
+                <button class="btn btn-primary" type="submit">
                     {{ trans('global.save') }}
                 </button>
             </div>
         </form>
     </div>
 </div>
-
-
 
 @endsection
