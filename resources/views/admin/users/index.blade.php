a66bfa205d2c8e77cf69f2457afcaa3b7041bb32
Angga Pratama <angga.pratama579@gmail.com>
2025-08-26 04:43:27 +0000

    Update UI v2 roles and users

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,93 +1,75 @@
 @extends('layouts.admin')
 @section('content')
-@can('user_create')
-    <div style="margin-bottom: 10px;" class="row">
-        <div class="col-lg-12">
-            <a class="btn btn-success" href="{{ route('admin.users.create') }}">
-                {{ trans('global.add') }} {{ trans('cruds.user.title_singular') }}
-            </a>
-        </div>
-    </div>
-@endcan
-<div class="card">
-    <div class="card-header">
-        {{ trans('cruds.user.title_singular') }} {{ trans('global.list') }}
-    </div>
 
+{{-- Bagian Header --}}
+<div class="d-flex justify-content-between align-items-center mb-4">
+    <h3 class="font-weight-bold">{{ trans('cruds.user.title') }}</h3>
+    @can('user_create')
+        <a class="btn btn-primary" href="{{ route('admin.users.create') }}">
+            <i class="fas fa-plus-circle me-2"></i> {{ trans('global.add') }} {{ trans('cruds.user.title_singular') }}
+        </a>
+    @endcan
+</div>
+
+{{-- Tabel Modern --}}
+<div class="card border-0 shadow-sm">
     <div class="card-body">
         <div class="table-responsive">
-            <table class=" table table-bordered table-striped table-hover datatable datatable-User">
+            <table class="modern-table datatable datatable-User">
                 <thead>
                     <tr>
-                        <th width="10">
-
-                        </th>
-                        <th>
-                            {{ trans('cruds.user.fields.id') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.user.fields.name') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.user.fields.email') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.user.fields.email_verified_at') }}
-                        </th>
-                        <th>
-                            {{ trans('cruds.user.fields.roles') }}
-                        </th>
-                        <th>
-                            &nbsp;
-                        </th>
+                        <th width="10"></th> {{-- Checkbox --}}
+                        <th><i class="fas fa-user"></i>{{ trans('cruds.user.fields.name') }}</th>
+                        <th><i class="fas fa-envelope"></i>{{ trans('cruds.user.fields.email') }}</th>
+                        <th class="text-center"><i class="fas fa-check-circle"></i>{{ trans('cruds.user.fields.email_verified_at') }}</th>
+                        <th><i class="fas fa-briefcase"></i>{{ trans('cruds.user.fields.roles') }}</th>
+                        <th class="text-center" style="width: 150px;"><i class="fas fa-cogs"></i>Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach($users as $key => $user)
                         <tr data-entry-id="{{ $user->id }}">
+                            <td></td> {{-- Checkbox --}}
                             <td>
-
+                                <div class="kegiatan-title-cell">{{ $user->name ?? '' }}</div>
+                                <div class="kegiatan-sub-cell">ID: {{ $user->id ?? '' }}</div>
                             </td>
                             <td>
-                                {{ $user->id ?? '' }}
+                                <div class="kegiatan-sub-cell">{{ $user->email ?? '' }}</div>
+                            </td>
+                            <td class="text-center">
+                                @if($user->email_verified_at)
+                                    <span class="badge-status badge-status-aktif">Terverifikasi</span>
+                                @else
+                                    <span class="badge-status badge-status-tidak-aktif">Belum</span>
+                                @endif
                             </td>
                             <td>
-                                {{ $user->name ?? '' }}
+                                <div class="permission-badges-container">
+                                    @foreach($user->roles as $key => $item)
+                                        <span class="badge badge-permission">{{ $item->title }}</span>
+                                    @endforeach
+                                </div>
                             </td>
-                            <td>
-                                {{ $user->email ?? '' }}
-                            </td>
-                            <td>
-                                {{ $user->email_verified_at ?? '' }}
-                            </td>
-                            <td>
-                                @foreach($user->roles as $key => $item)
-                                    <span class="badge badge-info">{{ $item->title }}</span>
-                                @endforeach
-                            </td>
-                            <td>
+                            <td class="text-center actions-cell">
                                 @can('user_show')
-                                    <a class="btn btn-xs btn-primary" href="{{ route('admin.users.show', $user->id) }}">
-                                        {{ trans('global.view') }}
+                                    <a class="btn btn-xs btn-info" href="{{ route('admin.users.show', $user->id) }}" title="Detail">
+                                        <i class="fas fa-eye"></i>
                                     </a>
                                 @endcan
-
                                 @can('user_edit')
-                                    <a class="btn btn-xs btn-info" href="{{ route('admin.users.edit', $user->id) }}">
-                                        {{ trans('global.edit') }}
+                                    <a class="btn btn-xs btn-success" href="{{ route('admin.users.edit', $user->id) }}" title="Edit">
+                                        <i class="fas fa-edit"></i>
                                     </a>
                                 @endcan
-
                                 @can('user_delete')
                                     <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" onsubmit="return confirm('{{ trans('global.areYouSure') }}');" style="display: inline-block;">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.delete') }}">
+                                        <button type="submit" class="btn btn-xs btn-danger" title="Hapus"><i class="fas fa-trash"></i></button>
                                     </form>
                                 @endcan
-
                             </td>
-
                         </tr>
                     @endforeach
                 </tbody>
@@ -95,57 +77,97 @@
         </div>
     </div>
 </div>
-
-
-
 @endsection
+
 @section('scripts')
 @parent
 <script>
     $(function () {
-  let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
-@can('user_delete')
-  let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
-  let deleteButton = {
-    text: deleteButtonTrans,
-    url: "{{ route('admin.users.massDestroy') }}",
-    className: 'btn-danger',
-    action: function (e, dt, node, config) {
-      var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
-          return $(entry).data('entry-id')
+      let dtButtons = $.extend(true, [], $.fn.dataTable.defaults.buttons)
+      @can('user_delete')
+      let deleteButtonTrans = '{{ trans('global.datatables.delete') }}'
+      let deleteButton = {
+        text: deleteButtonTrans,
+        url: "{{ route('admin.users.massDestroy') }}",
+        className: 'btn-danger',
+        action: function (e, dt, node, config) {
+          var ids = $.map(dt.rows({ selected: true }).nodes(), function (entry) {
+              return $(entry).data('entry-id')
+          });
+
+          if (ids.length === 0) {
+            Swal.fire('Peringatan', '{{ trans('global.datatables.zero_selected') }}', 'warning')
+            return
+          }
+
+          Swal.fire({
+            title: '{{ trans('global.areYouSure') }}',
+            text: "Data yang dipilih akan dihapus!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Ya, hapus!',
+            cancelButtonText: 'Batal'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              $.ajax({
+                headers: {'x-csrf-token': _token},
+                method: 'POST',
+                url: config.url,
+                data: { ids: ids, _method: 'DELETE' }})
+                .done(function () { location.reload() })
+            }
+          })
+        }
+      }
+      dtButtons.push(deleteButton)
+      @endcan
+
+      let table = $('.datatable-User').DataTable({
+        buttons: dtButtons,
+        order: [[ 1, 'asc' ]],
+        pageLength: 50,
+        columnDefs: [
+            {
+                orderable: false,
+                className: 'select-checkbox',
+                targets: 0
+            }, 
+            {
+                orderable: false,
+                searchable: false,
+                targets: -1
+            }
+        ],
+        select: {
+            style: 'multi+shift',
+            selector: 'td:first-child'
+        },
+        dom: 'lBfrtip'
+      });
+      
+      $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
+          $($.fn.dataTable.tables(true)).DataTable()
+              .columns.adjust();
       });
 
-      if (ids.length === 0) {
-        alert('{{ trans('global.datatables.zero_selected') }}')
+      $('.datatable-User').on('draw.dt', function () {
+          var wrapper = $(this).closest('.dataTables_wrapper');
+          var length = wrapper.find('.dataTables_length');
+          var filter = wrapper.find('.dataTables_filter');
+          var buttons = wrapper.find('.dt-buttons');
 
-        return
-      }
+          if (!wrapper.find('.dt-controls-row').length) {
+              var controlsRow = $('<div class="dt-controls-row"></div>');
+              var leftCol = $('<div class="dt-controls-left"></div>').append(length).append(buttons);
+              var rightCol = $('<div class="dt-controls-right"></div>').append(filter);
+              controlsRow.append(leftCol).append(rightCol);
+              wrapper.prepend(controlsRow);
+          }
+      });
 
-      if (confirm('{{ trans('global.areYouSure') }}')) {
-        $.ajax({
-          headers: {'x-csrf-token': _token},
-          method: 'POST',
-          url: config.url,
-          data: { ids: ids, _method: 'DELETE' }})
-          .done(function () { location.reload() })
-      }
-    }
-  }
-  dtButtons.push(deleteButton)
-@endcan
-
-  $.extend(true, $.fn.dataTable.defaults, {
-    orderCellsTop: true,
-    order: [[ 1, 'desc' ]],
-    pageLength: 50,
-  });
-  let table = $('.datatable-User:not(.ajaxTable)').DataTable({ buttons: dtButtons })
-  $('a[data-toggle="tab"]').on('shown.bs.tab click', function(e){
-      $($.fn.dataTable.tables(true)).DataTable()
-          .columns.adjust();
-  });
-  
-})
-
+      table.draw();
+    });
 </script>
 @endsection
--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -1,73 +1,61 @@
 @extends('layouts.admin')
 @section('content')
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.show') }} {{ trans('cruds.user.title') }}
-    </div>
-
-    <div class="card-body">
-        <div class="form-group">
-            <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.users.index') }}">
-                    {{ trans('global.back_to_list') }}
-                </a>
+<div class="card detail-view-card">
+    <div class="detail-header">
+        <div class="d-flex justify-content-between align-items-center">
+            <div>
+                <h2 class="detail-title">{{ $user->name }}</h2>
+                <p class="detail-sub-title mb-0">Detail untuk pengguna.</p>
             </div>
-            <table class="table table-bordered table-striped">
-                <tbody>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.id') }}
-                        </th>
-                        <td>
-                            {{ $user->id }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.name') }}
-                        </th>
-                        <td>
-                            {{ $user->name }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.email') }}
-                        </th>
-                        <td>
-                            {{ $user->email }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.email_verified_at') }}
-                        </th>
-                        <td>
-                            {{ $user->email_verified_at }}
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>
-                            {{ trans('cruds.user.fields.roles') }}
-                        </th>
-                        <td>
-                            @foreach($user->roles as $key => $roles)
-                                <span class="label label-info">{{ $roles->title }}</span>
-                            @endforeach
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="form-group">
-                <a class="btn btn-default" href="{{ route('admin.users.index') }}">
-                    {{ trans('global.back_to_list') }}
-                </a>
+            <a href="{{ route('admin.users.index') }}" class="btn btn-secondary">
+                <i class="fas fa-arrow-left me-2"></i> Kembali ke Daftar
+            </a>
+        </div>
+    </div>
+    <div class="card-body p-4">
+        <div class="detail-item">
+            <div class="icon"><i class="fas fa-id-card"></i></div>
+            <div class="content">
+                <div class="label">{{ trans('cruds.user.fields.id') }}</div>
+                <div class="value">{{ $user->id }}</div>
+            </div>
+        </div>
+        <div class="detail-item">
+            <div class="icon"><i class="fas fa-user"></i></div>
+            <div class="content">
+                <div class="label">{{ trans('cruds.user.fields.name') }}</div>
+                <div class="value">{{ $user->name }}</div>
+            </div>
+        </div>
+        <div class="detail-item">
+            <div class="icon"><i class="fas fa-envelope"></i></div>
+            <div class="content">
+                <div class="label">{{ trans('cruds.user.fields.email') }}</div>
+                <div class="value">{{ $user->email }}</div>
+            </div>
+        </div>
+        <div class="detail-item">
+            <div class="icon"><i class="fas fa-check-circle"></i></div>
+            <div class="content">
+                <div class="label">{{ trans('cruds.user.fields.email_verified_at') }}</div>
+                <div class="value">{{ $user->email_verified_at ?? 'Belum Diverifikasi' }}</div>
+            </div>
+        </div>
+        <div class="detail-item">
+            <div class="icon"><i class="fas fa-briefcase"></i></div>
+            <div class="content">
+                <div class="label">{{ trans('cruds.user.fields.roles') }}</div>
+                <div class="value permission-badges-container">
+                    @forelse($user->roles as $key => $item)
+                        <span class="badge badge-permission">{{ $item->title }}</span>
+                    @empty
+                        <span>Tidak ada peran yang diberikan.</span>
+                    @endforelse
+                </div>
             </div>
         </div>
     </div>
 </div>
-
-
 
 @endsection
