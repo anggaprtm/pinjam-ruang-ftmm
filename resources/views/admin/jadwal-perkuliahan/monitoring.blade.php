fa630fd4510be745ef93d8e72f23579b41f525ba
Angga Pratama <angga.iryanto@staf.unair.ac.id>
2026-02-11 07:58:10 +0700

    ✨feat: tambah kapasitas ruang di monitoring

--- resources/views/admin/jadwal-perkuliahan/monitoring.blade.php
+++ resources/views/admin/jadwal-perkuliahan/monitoring.blade.php
@@ -180,7 +180,7 @@
                 @foreach($ruangans as $ruangan)
                     <div class="room-row">
                         <div class="room-name">
-                            {{ $ruangan->nama }}
+                            {{ $ruangan->nama }} ({{ $ruangan->kapasitas }})
                         </div>
                         
                         <div class="room-timeline">
